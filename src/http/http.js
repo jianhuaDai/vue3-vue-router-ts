2b3204ff60181fc5d6df2bd91b4c254b9b52e66c
daijianhua <1763237102@qq.com>
2020-09-17 18:24:32 +0800

    请求拦截和回调拦截新增 axis默认配置提取

--- src/http/http.js
+++ src/http/http.js
@@ -1,164 +1,179 @@
 import axios from "axios";
-import config from "./config.js";
-import storage from "./storage.js";
-import qs from "qs";
-
 const CONF = {
-  timeout: 20000,
-  withCredentials: false,
+    timeout: 20000,
+    withCredentials: false,
 };
 
-const Http = {};
-
-//urlExtraParam 表示出token以外，需要额外拼在Url后面的参数
-//默认是一个对象，通过qs进行序列化后，拼接在Url后面
-Http.formatUrl = function(service, api, urlExtraParam = {}) {
-  let busId = storage.getCorpId("corpid"),
-    token = storage.getToken("token");
-
-  let extraParamStr = qs.stringify(urlExtraParam);
-  if (extraParamStr) extraParamStr = "&" + extraParamStr;
-
-  return (
-    "/" +
-    config.getConfig("service")[service] +
-    api.replace("{businessId}", busId) +
-    "?token=" +
-    token +
-    extraParamStr
-  );
-};
-
-Http.addCache = function(key, fn) {
-  return new Promise((resolve) => {
-    let val = sessionStorage.getItem(key);
-    if (val) {
-      resolve && resolve(JSON.parse(val));
-    } else if (fn) {
-      fn().then((resp) => {
-        sessionStorage.setItem(key, JSON.stringify(resp));
-        resolve && resolve(resp);
-      });
+// const Http = axios.create({
+//     baseURL: process.env.VUE_APP_API_BASE_URL, // api base_url
+//     timeout: 20000 // 请求超时时间
+// })
+const Http = axios.create(CONF)
+const err = (error) => {
+    if (error.response) {
+        const data = error.response.data
+        const token = Vue.ls.get(ACCESS_TOKEN)
+        if (error.response.status === 403) {
+            notification.error({
+                message: 'Forbidden',
+                description: data.msg
+            })
+        }
+        if (error.response.status === 401 && !(data.result && data.result.isLogin)) {
+            notification.error({
+                message: '提示',
+                description: '认证过期请重新登录'
+            })
+            if (token) {
+                store.dispatch('Logout').then(() => {
+                    setTimeout(() => {
+                        window.location.reload()
+                    }, 1000)
+                })
+            }
+        }
     }
-  });
+    return Promise.reject(error)
+}
+
+// request interceptor
+Http.interceptors.request.use(config => {
+    const token = Vue.ls.get(ACCESS_TOKEN)
+    if (token) {
+        config.headers['Authorization'] = token // 让每个请求携带自定义 token 请根据实际情况自行修改
+    }
+    return config
+}, err)
+
+// response interceptor
+Http.interceptors.response.use((response) => {
+    if (response.status === 200 && response.data && response.data.code === 0) {
+        return Promise.resolve(response.data)
+    } else {
+        notification.error({
+            message: response.data.msg
+        })
+        return Promise.reject(response)
+    }
+}, err)
+
+
+Http.post = function (url, param = {}, config, onlyData) {
+    config = Object.assign({}, config);
+    return new Promise((resolve, reject) => {
+        axios
+            .post(url, param, config)
+            .then((resp) => {
+                resolve && resolve(onlyData === false ? resp : resp.data);
+            })
+            .catch((error) => {
+                reject(error);
+            });
+    });
 };
 
-Http.post = function(url, param = {}, config, onlyData) {
-  config = Object.assign({}, CONF, config);
-  return new Promise((resolve, reject) => {
-    axios
-      .post(url, param, config)
-      .then((resp) => {
-        resolve && resolve(onlyData === false ? resp : resp.data);
-      })
-      .catch((error) => {
-        reject(error);
-      });
-  });
+Http.get = function (url, param = {}, config) {
+    config = Object.assign({}, config);
+    return new Promise((resolve, reject) => {
+        axios
+            .get(
+                url,
+                {
+                    params: param,
+                },
+                config
+            )
+            .then((resp) => {
+                resolve && resolve(resp.data);
+            })
+            .catch((err) => {
+                reject && reject(err);
+            });
+    });
 };
 
-Http.get = function(url, param = {}, config) {
-  config = Object.assign({}, CONF, config);
-  return new Promise((resolve, reject) => {
-    axios
-      .get(
-        url,
-        {
-          params: param,
+Http.delete = function (url, param = {}, config) {
+    config = Object.assign({}, config);
+    return new Promise((resolve, reject) => {
+        axios
+            .delete(
+                url,
+                {
+                    params: param,
+                },
+                config
+            )
+            .then((resp) => {
+                resolve && resolve(resp.data);
+            })
+            .catch((err) => {
+                reject && reject(err);
+            });
+    });
+};
+
+Http.put = function (url, param = {}, config) {
+    config = Object.assign({}, config);
+    return new Promise((resolve, reject) => {
+        axios
+            .put(url, param, config)
+            .then((resp) => {
+                resolve && resolve(resp.data);
+            })
+            .catch((err) => {
+                reject && reject(err);
+            });
+    });
+};
+
+Http.patch = function (url, param = {}, config) {
+    config = Object.assign({}, config);
+    return new Promise((resolve, reject) => {
+        axios
+            .patch(url, param, config)
+            .then((resp) => {
+                resolve && resolve(resp.data);
+            })
+            .catch((err) => {
+                reject && reject(err);
+            });
+    });
+};
+
+Http.form = function (url, formData, config) {
+    config = Object.assign({}, {
+        headers: {
+            "Content-Type": "multipart/form-data",
         },
-        config
-      )
-      .then((resp) => {
-        resolve && resolve(resp.data);
-      })
-      .catch((err) => {
-        reject && reject(err);
-      });
-  });
-};
-
-Http.delete = function(url, param = {}, config) {
-  config = Object.assign({}, CONF, config);
-  return new Promise((resolve, reject) => {
-    axios
-      .delete(
-        url,
-        {
-          params: param,
-        },
-        config
-      )
-      .then((resp) => {
-        resolve && resolve(resp.data);
-      })
-      .catch((err) => {
-        reject && reject(err);
-      });
-  });
-};
-
-Http.put = function(url, param = {}, config) {
-  config = Object.assign({}, CONF, config);
-  return new Promise((resolve, reject) => {
-    axios
-      .put(url, param, config)
-      .then((resp) => {
-        resolve && resolve(resp.data);
-      })
-      .catch((err) => {
-        reject && reject(err);
-      });
-  });
-};
-
-Http.patch = function(url, param = {}, config) {
-  config = Object.assign({}, CONF, config);
-  return new Promise((resolve, reject) => {
-    axios
-      .patch(url, param, config)
-      .then((resp) => {
-        resolve && resolve(resp.data);
-      })
-      .catch((err) => {
-        reject && reject(err);
-      });
-  });
-};
-
-Http.form = function(url, formData, config) {
-  config = Object.assign({}, CONF, {
-    headers: {
-      "Content-Type": "multipart/form-data",
-    },
-  });
-  return new Promise((resolve, reject) => {
-    axios
-      .post(url, formData, config)
-      .then((resp) => {
-        resolve && resolve(resp.data);
-      })
-      .catch((err) => {
-        reject && reject(err);
-      });
-  });
+    });
+    return new Promise((resolve, reject) => {
+        axios
+            .post(url, formData, config)
+            .then((resp) => {
+                resolve && resolve(resp.data);
+            })
+            .catch((err) => {
+                reject && reject(err);
+            });
+    });
 };
 
 //通用版
-Http.ajax = function(url, method = "get", params = {}, config = {}) {
-  config = Object.assign({}, CONF, config);
+Http.ajax = function (url, method = "get", params = {}, config = {}) {
+    config = Object.assign({}, config);
 
-  return axios({
-    url,
-    method,
-    params,
-    ...config,
-  })
-    .then((res) => {
-      return res.data;
+    return axios({
+        url,
+        method,
+        params,
+        ...config,
     })
-    .catch((err) => {
-      throw new Error(err);
-    });
+        .then((res) => {
+            return res.data;
+        })
+        .catch((err) => {
+            throw new Error(err);
+        });
 };
 
 export default Http;
